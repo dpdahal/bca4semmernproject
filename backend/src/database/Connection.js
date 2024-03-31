f84f80289e74f6c2ae7e866493f343038ded08b3
dpdahal <dp48dahal@gmail.com>
2024-03-31 10:21:41 +0545

    update

---
+++ backend/src/database/Connection.js
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+class Connection{
+    constructor(){
+        try{
+            mongoose.connect(process.env.DB_URL);
+            console.log('Database connected successfully');
+        }catch(err){
+            console.log('Error while connecting to database');
+            console.log(err);
+        }
+    }
+}
+export default Connection;
